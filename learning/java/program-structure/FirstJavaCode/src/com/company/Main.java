c0cf418c13480f82e242f74f685148a5f4524476
Reechika <jain.reechika@gmail.com>
2021-07-27 20:46:06 +0530

    learned about java program structure

---
+++ learning/java/program-structure/FirstJavaCode/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/** DOCUMENTATION
+    Author : Reechika Jain
+    Created on : 27-07-2021
+    company : rbr
+
+    this program is my first program in java . i am really excited about this.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// single line comment : i am using two variables a and b
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        if(sum == 30)
+            System.out.println("sum is 30");
+
+
+    }
+}
